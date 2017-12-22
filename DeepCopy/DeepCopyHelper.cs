6c19631d8122e5f01728cb85aa756682813748a5
ygzh2008 <ygzh2008@qq.com>
2017-12-22 15:25:01 +0800

    Shallow Copy & Deep Copy

---
+++ DeepCopy/DeepCopyHelper.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DeepCopy
+{
+    public class DeepCopyHelper
+    {
+        // 用一个字典来存放每个对象的反射次数来避免反射代码的循环递归
+        static Dictionary<Type, int> typereflectionCountDic = new Dictionary<Type, int>();
+        static object DeepCopyDemoClasstypeRef = null;
+
+        // 利用反射实现深拷贝
+        public static T DeepCopyWithReflection<T>(T obj)
+        {
+            Type type = obj.GetType();
+
+            // 如果是字符串或值类型则直接返回
+            if (obj is string || type.IsValueType) return obj;
+
+            if (type.IsArray)
+            {
+                Type elementType = Type.GetType(type.FullName.Replace("[]", string.Empty));
+                var array = obj as Array;
+                Array copied = Array.CreateInstance(elementType, array.Length);
+                for (int i = 0; i < array.Length; i++)
+                {
+                    copied.SetValue(DeepCopyWithReflection(array.GetValue(i)), i);
+                }
+
+                return (T)Convert.ChangeType(copied, obj.GetType());
+            }
+
+            object retval = Activator.CreateInstance(obj.GetType());
+            
+            PropertyInfo[] properties = obj.GetType().GetProperties(
+                BindingFlags.Public | BindingFlags.NonPublic
+                | BindingFlags.Instance | BindingFlags.Static);
+            foreach (var property in properties)
+            {
+                var propertyValue = property.GetValue(obj, null);
+                if (propertyValue == null)
+                    continue;
+                property.SetValue(retval, DeepCopyWithReflection(propertyValue), null);
+            }
+
+            return (T)retval;
+        }
+
+        public static T DeepCopyWithReflection_Second<T>(T obj)
+        {
+            Type type = obj.GetType();
+
+            // 如果是字符串或值类型则直接返回
+            if (obj is string || type.IsValueType) return obj;
+
+            if (type.IsArray)
+            {
+                Type elementType = Type.GetType(type.FullName.Replace("[]", string.Empty));
+                var array = obj as Array;
+                Array copied = Array.CreateInstance(elementType, array.Length);
+                for (int i = 0; i < array.Length; i++)
+                {
+                    copied.SetValue(DeepCopyWithReflection_Second(array.GetValue(i)), i);
+                }
+
+                return (T)Convert.ChangeType(copied, obj.GetType());
+            }
+
+            // 对于类类型开始记录对象反射的次数
+            int reflectionCount = Add(typereflectionCountDic, obj.GetType());
+            if (reflectionCount > 1)
+                return obj;
+
+            object retval = Activator.CreateInstance(obj.GetType());
+
+            PropertyInfo[] properties = obj.GetType().GetProperties(
+                BindingFlags.Public | BindingFlags.NonPublic
+                | BindingFlags.Instance | BindingFlags.Static);
+            foreach (var property in properties)
+            {
+                var propertyValue = property.GetValue(obj, null);
+                if (propertyValue == null)
+                    continue;
+                property.SetValue(retval, DeepCopyWithReflection_Second(propertyValue), null);
+            }
+
+            return (T)retval;
+        }
+
+        public static T DeepCopyWithReflection_Third<T>(T obj)
+        {
+            Type type = obj.GetType();
+
+            // 如果是字符串或值类型则直接返回
+            if (obj is string || type.IsValueType) return obj;
+
+            if (type.IsArray)
+            {
+                Type elementType = Type.GetType(type.FullName.Replace("[]", string.Empty));
+                var array = obj as Array;
+                Array copied = Array.CreateInstance(elementType, array.Length);
+                for (int i = 0; i < array.Length; i++)
+                {
+                    copied.SetValue(DeepCopyWithReflection_Second(array.GetValue(i)), i);
+                }
+
+                return (T)Convert.ChangeType(copied, obj.GetType());
+            }
+
+            int reflectionCount = Add(typereflectionCountDic, obj.GetType());
+            if (reflectionCount > 1 && obj.GetType() == typeof(DeepCopyDemoClass))
+                return (T)DeepCopyDemoClasstypeRef; // 返回deepCopyClassB对象
+
+            object retval = Activator.CreateInstance(obj.GetType());
+
+            if(retval.GetType() == typeof(DeepCopyDemoClass))
+                DeepCopyDemoClasstypeRef = retval; // 保存一开始创建的DeepCopyDemoClass对象
+
+            PropertyInfo[] properties = obj.GetType().GetProperties(
+                BindingFlags.Public | BindingFlags.NonPublic
+                | BindingFlags.Instance | BindingFlags.Static);
+            foreach (var property in properties)
+            {
+                var propertyValue = property.GetValue(obj, null);
+                if (propertyValue == null)
+                    continue;
+                property.SetValue(retval, DeepCopyWithReflection_Third(propertyValue), null);
+            }
+
+            return (T)retval;
+        }
+
+        private static T SetArrayObject<T>(T arrayObj)
+        {
+            Type elementType = Type.GetType(arrayObj.GetType().FullName.Replace("[]", string.Empty));
+            var array = arrayObj as Array;
+            Array copied = Array.CreateInstance(elementType, array.Length);
+            for (int i = 0; i < array.Length; i++)
+            {
+                copied.SetValue(DeepCopyWithReflection_Third(array.GetValue(i)), i);
+            }
+
+            return (T)Convert.ChangeType(copied, arrayObj.GetType());
+        }
+
+        private static int Add(Dictionary<Type, int> dict, Type key)
+        {
+            if (key.Equals(typeof(String)) || key.IsValueType) return 0;
+            if (!dict.ContainsKey(key))
+            {
+                dict.Add(key, 1);
+                return dict[key];
+            }
+
+            dict[key] += 1;
+            return dict[key];
+        }
+
+        // 利用XML序列化和反序列化实现
+        public static T DeepCopyWithXmlSerializer<T>(T obj)
+        {
+            object retval;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(T));
+                xml.Serialize(ms, obj);
+                ms.Seek(0, SeekOrigin.Begin);
+                retval = xml.Deserialize(ms);
+                ms.Close();
+            }
+
+            return (T)retval;
+        }
+
+        // 利用二进制序列化和反序列实现
+        public static T DeepCopyWithBinarySerialize<T>(T obj)
+        {
+            object retval;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                // 序列化成流
+                bf.Serialize(ms, obj);
+                ms.Seek(0, SeekOrigin.Begin);
+                // 反序列化成对象
+                retval = bf.Deserialize(ms);
+                ms.Close();
+            }
+
+            return (T)retval;
+        }
+
+        // 利用DataContractSerializer序列化和反序列化实现
+        public static T DeepCopy<T>(T obj)
+        {
+            object retval;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                DataContractSerializer ser = new DataContractSerializer(typeof(T));
+                ser.WriteObject(ms, obj);
+                ms.Seek(0, SeekOrigin.Begin);
+                retval = ser.ReadObject(ms);
+                ms.Close();
+            }
+            return (T)retval;
+        }
+        
+        // 表达式树实现
+        // ....
+    }
+}
---
+++ ShallowCopy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ShallowCopy
+{
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person sourceP = new Person() { Name = "张三" };
+            Person copyP = sourceP; // 浅拷贝
+            copyP.Name = "张老三"; // 拷贝对象改变Name值
+            // 结果都是"张老三",因为实现的是浅拷贝，一个对象的改变都会影响到另一个对象
+            Console.WriteLine("Person.Name: [SourceP: {0}] [CopyP:{1}]", sourceP.Name, copyP.Name);
+            Console.Read();
+        }
+    }
+}
